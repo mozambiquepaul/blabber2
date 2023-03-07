ba123e3528ed184780807ae803b19aeabb9ea045
mozambiquepaul <mozambiquepaul@gmail.com>
2023-03-07 10:50:32 -0500

    Update blabber2.py

--- blabber2.py
+++ blabber2.py
@@ -1,58 +1,61 @@
 import tweepy
-import os
 import openai
+import os
 
-# Load the Twitter API credentials and OpenAI API key
-consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
-access_token = os.environ['TWITTER_ACCESS_TOKEN']
-access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
-openai.api_key = os.environ['OPENAI_API_KEY']
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(os.environ["TWITTER_API_KEY"], os.environ["TWITTER_API_SECRET"])
+auth.set_access_token(os.environ["TWITTER_ACCESS_TOKEN"], os.environ["TWITTER_ACCESS_TOKEN_SECRET"])
 
-# Authenticate with the Twitter API
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+# Authenticate to OpenAI
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Define a function to respond to a tweet or direct message using the OpenAI API
-def respond_to_tweet(tweet):
-    # Use the OpenAI API to generate a response
-    prompt = "Reply to this tweet: " + tweet.text
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    ).choices[0].text.strip()
-    # Post the response to Twitter
-    api.update_status(
-        status=response,
-        in_reply_to_status_id=tweet.id,
-        auto_populate_reply_metadata=True,
-    )
 
-# Define a class that listens for incoming tweets and direct messages
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        super().__init__()
+
+    def on_status(self, tweet):
+        if tweet.user.id_str == self.api.me().id_str:
+            # Skip tweets that I have posted
+            return
+        try:
+            tweet_text = tweet.extended_tweet["full_text"]
+        except AttributeError:
+            tweet_text = tweet.text
+        prompt = f"Q: What's the sentiment of this tweet?\nA: The sentiment is"
+        prompt += " positive." if tweet.favorite_count > 0 else " negative."
+        prompt += f"\nQ: What's the topic of this tweet?\nA: The topic is {tweet.user.name}."
+        prompt += f"\nQ: Generate a reply to this tweet:\nA:"
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=60,
+            n=1,
+            stop=None,
+            temperature=0.5,
+            timeout=4,
+        )
+        reply_text = response.choices[0].text.strip()
+        reply_tweet = f"@{tweet.user.screen_name} {reply_text}"
+        print(f"Replying to {tweet.user.name}: {reply_tweet}")
+        try:
+            self.api.update_status(
+                status=reply_tweet,
+                in_reply_to_status_id=tweet.id_str,
+                auto_populate_reply_metadata=True,
+            )
+        except tweepy.TweepError as error:
+            print(f"Error: {error.reason}")
+
+
 class MyStream(tweepy.Stream):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.myStreamListener = MyStreamListener()
+        listener = MyStreamListener(kwargs.pop("api"))
+        super().__init__(listener=listener, *args, **kwargs)
 
-    def on_status(self, status):
-        if status.user.id != api.me().id:  # Ignore self-tweets
-            self.myStreamListener.respond_to_tweet(status)
 
-    def on_direct_message(self, message):
-        if message.sender_id != api.me().id:  # Ignore self-messages
-            self.myStreamListener.respond_to_tweet(message)
+if __name__ == "__main__":
+    myStream = MyStream(auth=auth, listener=None)
+    myStream.filter(track=["python", "javascript", "ruby"])
 
-class MyStreamListener:
-    def respond_to_tweet(self, tweet):
-        respond_to_tweet(tweet)
-
-# Create a stream to listen for incoming tweets and direct messages
-myStream = MyStream(auth=api.auth, listener=None)
-
-# Start listening for incoming tweets and direct messages
-myStream.filter(track=[api.me().screen_name], is_async=True)
